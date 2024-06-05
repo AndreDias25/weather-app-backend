39fe416f3dd2b9220a65fad429ef8ace14cd5364
André Dias <andre25dias@gmail.com>
2024-06-05 17:32:41 -0300

    :bugfix: Modificando o CORS

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(cors({
-//   origin: 'https://your-frontend-domain.com' // Substitua pelo domínio do seu frontend
-// }));
+const corsOptions = {
+  origin: ['https://angular-weather-app-tau-fawn.vercel.app', 'http://localhost:4200']
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/get-key', (req, res) => {
   const apiKey = process.env.API_KEY;
   res.json({ apiKey });
 });
 
-app.get('/api/get-weather', (req, res) => {
-  const apiKey = process.env.API_KEY;
-  // Faça a chamada à API da Accuweather aqui e envie a resposta de volta
-  // Você pode usar fetch ou axios para fazer essa requisição
-});
+// app.get('/api/get-weather', (req, res) => {
+//   const apiKey = process.env.API_KEY;
+  
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port} - Chave da API: ${process.env.API_KEY}`);
