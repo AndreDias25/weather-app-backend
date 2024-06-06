ae991efce694db158223cfa4688e91649f7a4a6e
André Dias <andre25dias@gmail.com>
2024-06-06 17:01:27 -0300

    :broom: Limpeza e refatoração do código

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const axios = require('axios');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const corsOptions = {
-//   origin: ['https://angular-weather-app-tau-fawn.vercel.app', 'http://localhost:4200']
-// };
 
 const corsOptions = {
   origin: ['http://localhost:4200', 'https://angular-weather-app-tau-fawn.vercel.app'],
@@ -25,11 +23,41 @@ app.get('/api/get-key', (req, res) => {
   res.json({ apiKey });
 });
 
-// app.get('/api/get-weather', (req, res) => {
-//   const apiKey = process.env.API_KEY;
-  
-// });
+app.get('/api/cities/autocomplete/:cityName', async (req, res) => {
+  let { cityName } = req.params;
+  cityName = cityName.replace(':', '');
+
+  try {
+    const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?q=${cityName}&apikey=${process.env.API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from AccuWeather API:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to fetch data from AccuWeather API' });
+  }
+});
+
+app.get('/api/currentconditions/:cityKey', async (req, res) => {
+  let { cityKey } = req.params;
+  cityKey = cityKey.replace(':', '');
+  try {
+    const response = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${process.env.API_KEY}&details=true`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from AccuWeather API' });
+  }
+});
+
+app.get('/api/forecasts/12hour/:cityKey', async (req, res) => {
+  let { cityKey } = req.params;
+  cityKey = cityKey.replace(':', '');
+  try {
+    const response = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityKey}?apikey=${process.env.API_KEY}&metric=true`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from AccuWeather API' });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port} - Chave da API: ${process.env.API_KEY}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
